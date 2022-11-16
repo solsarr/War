a8fb95ed41f86e469d1839fbe65952a3cc9f4ff1
solsarr <solomonsarr18@gmail.com>
2022-11-16 15:02:57 -0500

    created function to pull a random number and insert it into the div
    git status
    gti commit

--- main.js
+++ main.js
@@ -21,5 +21,45 @@ function ace() {
 // deal.addEventListener('click',console.log('hello'));
 window.onload = function (){
 const deal = document.getElementById("deal");
-deal.addEventListener("click", ace);
+deal.addEventListener("click", getImg);
 }
+function getImg() { 
+
+const randomImg = new Array('250','350');
+
+randomImg[0] = "https://www.improvemagic.com/wp-content/uploads/2020/11/pa.png";
+
+randomImg[1] = 'https://www.improvemagic.com/wp-content/uploads/2020/11/p2.png';
+
+randomImg[2] = 'https://www.improvemagic.com/wp-content/uploads/2020/11/p3.png';
+
+randomImg[3] = 'https://www.improvemagic.com/wp-content/uploads/2020/11/p4.png';
+
+randomImg[4] = 'https://www.improvemagic.com/wp-content/uploads/2020/11/p5.png';
+
+randomImg[5] = 'https://www.improvemagic.com/wp-content/uploads/2020/11/p6.png';
+
+randomImg[6] = 'https://www.improvemagic.com/wp-content/uploads/2020/11/p7.png';
+
+randomImg[7] = 'https://www.improvemagic.com/wp-content/uploads/2020/11/p8.png';
+
+randomImg[8] = 'https://www.improvemagic.com/wp-content/uploads/2020/11/p9.png';
+
+randomImg[9] = 'https://www.improvemagic.com/wp-content/uploads/2020/11/p10.png';
+
+randomImg[10] = 'https://www.improvemagic.com/wp-content/uploads/2020/11/pj.png';
+
+randomImg[11] = 'https://www.improvemagic.com/wp-content/uploads/2020/11/pq.png';
+
+randomImg[12] = 'https://www.improvemagic.com/wp-content/uploads/2020/11/pk.png';
+
+const imgNum = Math.floor(Math.random()*randomImg.length);
+
+return document.getElementById("pCard").innerHTML = '<img src="'+randomImg[imgNum]+'" />';  
+
+
+// return document.getElementById("pCard").appendChild(randomImg[imgNum]);
+
+
+}
+console.log(getImg)
